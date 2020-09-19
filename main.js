8b3c6e87ace54daf2102977ec86e8fabfb1379d4
code express <codeexpress333@gmail.com>
2020-09-19 11:57:28 +0530

    text animation

---
+++ main.js
@@ -0,0 +1,20 @@
+//Get Text
+var textWrapper = document.querySelector('.text .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+//Now Animation Start
+anime.timeline({ loop: true })
+    .add({
+        targets: '.text .letter',
+        scale: [0, 1],
+        duration: 1500,
+        elasticity: 600,
+        delay: (el, i) => 45 * (i + 1)
+    }).add({
+        targets: '.text',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
